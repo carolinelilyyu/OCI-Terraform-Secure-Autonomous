e4752078820af5e647c114303bd2f8dcf72c9a9b
Caroline L. Yu <clyu@Carolines-MacBook-Pro-2.local>
2019-05-03 09:56:13 -0400

    cleaned

--- modules/network/vars.tf
+++
@@ -1,9 +0,0 @@
-# Variables passed into network
-variable "tenancy_ocid" {}
-
-variable "compartment_ocid" {}
-variable "ssh_public_key" {}
-variable "vcn_display_name" {}
-variable "availability_domain" {}
-variable "destination" {}
-variable "service_gateway_id" {}
