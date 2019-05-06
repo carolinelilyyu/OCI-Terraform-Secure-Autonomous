1ffc5bbadc9ec35da444f23c1fce2859a1598452
Caroline L. Yu <clyu@Carolines-MacBook-Pro-2.local>
2019-05-06 16:16:03 -0400

    got rid of creds

--- terraform/env.sh
+++ terraform/env.sh
@@ -1,13 +1,13 @@
 #Define authentication variables
 
 #Enter your tenancy OCID
-export TF_VAR_tenancy_ocid="ocid1.tenancy.oc1..aaaaaaaaa5jn745fyrmpizutz5q2gjjduhot4ny2wxda7fobvkeu5wkpeysa"
+export TF_VAR_tenancy_ocid=""
 #Enter your region (e.g. us-ashburn-1)
 export TF_VAR_region="us-ashburn-1"
 #Enter your user OCID
-export TF_VAR_user_ocid="ocid1.user.oc1..aaaaaaaaes2w2nmb5z7y27qjgdo5pzwxdafcahbvytzyfw23xbsawfqxcgia"
+export TF_VAR_user_ocid=""
 #Fingerprint
-export TF_VAR_fingerprint="59:a2:af:2f:cd:eb:a1:a2:85:82:67:88:db:fc:db:dc"
+export TF_VAR_fingerprint=""
 #Path to your private API key
 export TF_VAR_oci_private_key="../.oci/oci_api_key.pem"
 #Path to your public API key
