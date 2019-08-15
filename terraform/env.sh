9d4412bd3b5b2ee066119e467ed21ef0abd004b5
Steven Riley <rileystevend@gmail.com>
2019-08-15 13:21:25 -0500

    updated env variables

--- terraform/env.sh
+++ terraform/env.sh
@@ -7,7 +7,7 @@ export TF_VAR_region="us-ashburn-1"
 #Enter your user OCID
 export TF_VAR_user_ocid="ocid1.user.oc1..aaaaaaaapojltszogsvqwohv6c6zofuhahp754qwgospfamkjefkfn37x24a"
 #Fingerprint
-export TF_VAR_fingerprint=""
+export TF_VAR_fingerprint="59:a2:af:2f:cd:eb:a1:a2:85:82:67:88:db:fc:db:dc"
 #Developer image ocid
 export TF_VAR_developer_image_ocid="ocid1.image.oc1..aaaaaaaanqr276n5ia2hgmjr25gvdn3ejtok4apli2yecbqobmkyve5v47mq"
 #Path to your private API key
